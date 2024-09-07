aed68b43402b78de757d1038bc992b8143819c17
brotSchimmelt <tknebler+git@googlemail.com>
2024-09-07 19:20:56 +0200

    refactor: simplify script and remove unused parts

--- my_extract_diff.py
+++ my_extract_diff.py
@@ -2,17 +2,12 @@ import argparse
 import json
 import logging
 import os
-import re
 import time
 
-import numpy as np
-import pylatexenc
-from nltk.tokenize import word_tokenize
-from pylatexenc.latexwalker import LatexWalker
 from tqdm import tqdm
 
-from src.settings import DOMAINS, WIKIPEDIA_MAIN_CATEGORIES
-from src.utils import clean_text, clean_unused
+from src.settings import DOMAINS
+from src.utils import clean_text
 
 
 def parse_arguments() -> argparse.Namespace:
@@ -25,41 +20,15 @@ def parse_arguments() -> argparse.Namespace:
         type=str,
         help="Specify document domain, please select from: wikipedia or wikinews",
     )
-    parser.add_argument(
-        "--main_category",
-        type=str,
-        help="Specify main category. For wikinews use 'all'.",
-    )
     args = parser.parse_args()
 
     if args.domain not in DOMAINS:
         raise ValueError(f"Invalid domain: {args.domain} | {DOMAINS}")
 
-    # check if main category is aligns with settings.py
-    if args.main_category is None:
-        if args.domain == "wikipedia":
-            if args.main_category not in WIKIPEDIA_MAIN_CATEGORIES:
-                raise ValueError(f"Invalid main category: {args.main_category}")
-        elif args.domain == "wikinews":
-            args.main_category = "all"
-
     return args
 
 
-def remove_empty(edit_list):
-    """
-    Remove empty strings from a list.
-
-    Args:
-        edit_list (list): List from which to remove empty strings.
-
-    Returns:
-        list: A list with all empty strings removed.
-    """
-    return [item for item in edit_list if item]
-
-
-def write_to_latex(tmp_path, content, file_name):
+def write_to_latex(tmp_path: str, content: str, file_name: str) -> str:
     """
     Write a given content to a LaTeX file format.
 
@@ -85,7 +54,9 @@ def write_to_latex(tmp_path, content, file_name):
     return file_path
 
 
-def generate_latex_diff(doc_id, ver_id, before_revision, after_revision, tmp_path):
+def generate_latex_diff(
+    doc_id: str, ver_id: int, before_revision: str, after_revision: str, tmp_path: str
+) -> None:
     """
     Generate a LaTeX diff file between two revisions using latexdiff.
 
@@ -116,134 +87,7 @@ def generate_latex_diff(doc_id, ver_id, before_revision, after_revision, tmp_pat
     os.remove(target_file)
 
 
-def extract_diffs_from_latex(file_path):
-    """
-    Extract meaningful diffs from a LaTeX diff output file.
-
-    Args:
-        file_path (str): Path to the LaTeX diff file.
-
-    Returns:
-        str: Extracted abstract containing diffs.
-    """
-    with open(file_path, "r") as f:
-        latex = f.read()
-    search_obj = re.search(r"begin{abstract}", latex)
-    sid = search_obj.end() if search_obj else 0
-    abstract = latex[sid:].strip("[ \n]")
-    abstract = re.sub(r"\$", "MATH", abstract)
-    abstract = re.sub(r"\\newcite{.*}", "CITATION", abstract)
-
-    paras = abstract.split("\n\n")
-    return "\nNEW_PARAGRAPH\n".join(para for para in paras if "DIFdel" in para or "DIFadd" in para)
-
-
-def extract_raw_text(diff_out, mode="DIFdel"):
-    """
-    Extract raw text content marked by DIFadd or DIFdel tags from the diff output.
-
-    Args:
-        diff_out (list): List containing the diff output.
-        mode (str): Mode to extract ('DIFdel' or 'DIFadd').
-
-    Returns:
-        str: Extracted raw text based on the specified mode.
-    """
-    abstract = []
-    pos_arr = np.zeros(len(diff_out))
-
-    if not diff_out:
-        return ""
-
-    for i, diff in enumerate(diff_out):
-        if diff in ["DIFdel", "DIFadd"]:
-            if i + 1 < len(diff_out):  # ensure i+1 is within bounds
-                if diff == mode:
-                    abstract.append(diff_out[i + 1])
-                    pos_arr[i + 1] = 1
-                else:
-                    pos_arr[i + 1] = -1
-        elif pos_arr[i] == 0:
-            abstract.append(diff)
-
-    return " ".join(abstract)
-
-
-def parse_diffs(ID, ver_id, tmp_path):
-    """
-    Parse the diffs from a LaTeX diff output file and extract information.
-
-    Args:
-        ID (str): Document ID.
-        ver_id (int): Version ID for the current revision.
-        tmp_path (str): Path to the temporary directory.
-
-    Returns:
-        dict: Parsed diff data including raw text and lists of additions and deletions.
-    """
-    diff_file_path = os.path.join(tmp_path, f"{ID}_diff_v{ver_id}v{ver_id+1}.tex")
-    diff_abs = extract_diffs_from_latex(diff_file_path)
-    nodes, pos, len_ = LatexWalker(diff_abs).get_latex_nodes(pos=0)
-
-    all_nodes = []
-    for i, node in enumerate(nodes):
-        if node.isNodeType(pylatexenc.latexwalker.LatexGroupNode):
-            prev_node = nodes[i - 1]
-            if prev_node.isNodeType(
-                pylatexenc.latexwalker.LatexMacroNode
-            ) and prev_node.macroname in ["DIFdel", "DIFadd"]:
-                for j, char_node in enumerate(node.nodelist):
-                    if char_node.isNodeType(pylatexenc.latexwalker.LatexCharsNode):
-                        all_nodes += [prev_node, char_node]
-                    elif char_node.isNodeType(pylatexenc.latexwalker.LatexGroupNode):
-                        for k, cchar_node in enumerate(char_node.nodelist):
-                            if cchar_node.isNodeType(pylatexenc.latexwalker.LatexCharsNode):
-                                prev_char_node = char_node.nodelist[k - 1]
-                                if prev_char_node.isNodeType(
-                                    pylatexenc.latexwalker.LatexMacroNode
-                                ) and prev_char_node.macroname in ["DIFdel", "DIFadd"]:
-                                    all_nodes += [prev_char_node, cchar_node]
-                                else:
-                                    all_nodes += [prev_node, cchar_node]
-        if node.isNodeType(pylatexenc.latexwalker.LatexCharsNode):
-            all_nodes += [node]
-
-    diff_out, delete_list, add_list = [], [], []
-    for i, node in enumerate(all_nodes):
-        if node.isNodeType(pylatexenc.latexwalker.LatexCharsNode):
-            chars = node.chars.replace("MATH", "").replace("DIFdelbegin", "")
-            char_str = " ".join(word_tokenize(chars))
-            if char_str:
-                diff_out.append(char_str.replace("NEW_PARAGRAPH", "\n**NEW_PARAGRAPH**\n"))
-        elif node.isNodeType(pylatexenc.latexwalker.LatexMacroNode):
-            if node.macroname == "DIFdel":
-                diff_out.append(node.macroname)
-                chars = all_nodes[i + 1].chars.replace("MATH", "").replace("DIFdelbegin", "")
-                delete_list.append(clean_unused(" ".join(word_tokenize(chars))))
-            elif node.macroname == "DIFadd":
-                diff_out.append(node.macroname)
-                chars = all_nodes[i + 1].chars.replace("MATH", "").replace("DIFdelbegin", "")
-                add_list.append(clean_unused(" ".join(word_tokenize(chars))))
-
-    delete_list = remove_empty(delete_list)
-    add_list = remove_empty(add_list)
-
-    source_abs = extract_raw_text(diff_out, mode="DIFdel")
-    target_abs = extract_raw_text(diff_out, mode="DIFadd")
-
-    if not diff_out:
-        return {}
-
-    return {
-        "before_raw_txt": source_abs,
-        "after_raw_txt": target_abs,
-        "diff_out": diff_out,
-        "delete_list": delete_list,
-        "add_list": add_list,
-    }
-
-
-def parse_diff(directory, domain):
+def create_diffs(directory: str, domain: str) -> None:
     """
     Parse diffs for all files in a specified directory.
 
@@ -251,8 +95,9 @@ def parse_diff(directory, domain):
         directory (str): Path to the directory containing revision files.
         domain (str): Domain name (e.g., 'wikipedia' or 'wikinews').
     """
-    tmp_path = os.path.join(directory, f"latexdiff_{domain}")
-    os.makedirs(tmp_path, exist_ok=True)
+    # path to save the temporary latexdiff files
+    latexdiff_path = os.path.join("./data/extracted_revisions", f"latexdiff_{domain}")
+    os.makedirs(latexdiff_path, exist_ok=True)
 
     files = [f for f in os.listdir(directory) if domain in f and f.endswith(".json")]
 
@@ -269,12 +114,14 @@ def parse_diff(directory, domain):
             before_revision = clean_text(line_data["before_revision"], domain)
             after_revision = clean_text(line_data["after_revision"], domain)
 
-            generate_latex_diff(doc_id, version_depth, before_revision, after_revision, tmp_path)
-            parsed_data = parse_diffs(doc_id, version_depth, tmp_path)
+            # generate latexdiff files for every single revision in the merged data
+            generate_latex_diff(
+                doc_id, version_depth, before_revision, after_revision, latexdiff_path
+            )  # store them to latexdiff_path
 
 
 def main(args: argparse.Namespace) -> None:
-    parse_diff(directory="./data/extracted_revisions", domain=args.domain)
+    create_diffs(directory=f"./data/{args.domain}/merged", domain=args.domain)
 
 
 if __name__ == "__main__":
